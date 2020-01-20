fe9d09bf0f92e42f168261205ee355e5ec8961a4
upcvig <alex.rios.jerez@gmail.com>
2020-01-20 19:46:45 +0100

    Segona versió: MoveBehaviour sense RigidBody

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private MoveBehaviour _movebehaviour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movebehaviour = GetComponent<MoveBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movebehaviour.move();
+    }
+}
---
+++ Assets/Scripts/MoveBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _direction;
+
+    [SerializeField]
+    private float _velocity;
+    public void move()
+    {
+        transform.position = new Vector3(transform.position.x + _velocity * Time.deltaTime*_direction.x,
+                                       transform.position.y + _velocity * Time.deltaTime*_direction.y,
+                                       0);
+    }
+}
