df7261aa1fe02056ae36b6006bbb607d9994d501
upcvig <alex.rios.jerez@gmail.com>
2020-01-24 17:31:37 +0100

    Revert "PoolingAfegit"
    This reverts commit ff1d6678925cf4088f856bfc410114b1bb43a595.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,35 +7,12 @@ public class Player : MonoBehaviour
     private MoveBehaviour _moveBehaviour;
     // Update is called once per frame
 
-    [SerializeField]
-    private GameObject _projectile;
-
-    [SerializeField]
-    private Transform _shotPoint;
     private void Awake()
     {
         _moveBehaviour = GetComponent<MoveBehaviour>();
     }
-
-    public void Start()
+    public void FixedUpdate()
     {
-        StartCoroutine(DoSpawnShots());
-    }
-    public void Update()
-    {
-       // if (Input.GetKeyDown(KeyCode.Space))
-            
-    }
-
-    private IEnumerator DoSpawnShots()
-    {
-        yield return new WaitForSeconds(0.15f);
-        ObjectPoolingManager.Instance.InstantiatePoolItem(
-                _projectile,
-                _shotPoint.position,
-                Quaternion.identity);
         
-        yield return DoSpawnShots();
-
     }
 }
